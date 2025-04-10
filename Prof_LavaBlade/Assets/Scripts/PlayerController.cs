5bf2a7df8e8341725db0af963e76611ce5e339a4
Sebastien Siva <ssiva@ggc.edu>
2025-04-10 16:24:26 -0400

    Mid Lecture

--- Prof_LavaBlade/Assets/Scripts/PlayerController.cs
+++ Prof_LavaBlade/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class PlayerController : MonoBehaviourPunCallbacks
+public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
 {
     //Connected Components
     Rigidbody myBod;
@@ -16,43 +16,66 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
     public int health;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Awake() {
         myBod = GetComponent<Rigidbody>();
         myCol = GetComponent<Collider>();
         myHealthBar = transform.Find("Canvas/GreenHealth").GetComponent<Transform>();
         myNameTxt = transform.Find("Canvas/NamePlate").GetComponent<Text>();
         myScoreTxt = transform.Find("Canvas/ScorePlate").GetComponent<Text>();
+    }
 
+    // Start is called before the first frame update
+    void Start()
+    {
         myNameTxt.text = photonView.Owner.NickName;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(health <= 0) {
-            //dead
-            myBod.velocity = Vector3.zero;
-            myCol.enabled = false;
-            myNameTxt.color = Color.gray;
-            myScoreTxt.color = Color.gray;
-        } else if(photonView.IsMine) {
+        //I AM THE OWNER OF THIS CUBE
+        if(photonView.IsMine) {
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
             myBod.AddForce(500 * (new Vector3(h, v, 0)) * Time.deltaTime);
             // tell the world where I (the owner) sent my cube.
             // send both position and velocity
-            // this is done automatically by photon rigid body oberserver component
+            // this is done automatically by photonRigidbodyView component
         }
+        //THIS IS SOMEONE ELSE'S CUBE
         else {
-            // recieve position from photon rigid body oberserver
+            //recieve position and velocity
+            //update cube
+            // this is done automatically by photonRigidbodyView component
         }
-        
+
+        //ALL CUBES
+        myHealthBar.localScale = new Vector3(health / 100f, 1, 1);
+        if(health <= 0) {
+            //dead
+            health = 0;
+            myBod.velocity = Vector3.zero;
+            myBod.constraints = RigidbodyConstraints.FreezeAll;
+            myCol.enabled = false;
+            myNameTxt.color = Color.gray;
+            myScoreTxt.color = Color.gray;
+        }         
     }
 
     void OnTriggerStay(Collider other) {
-        health--;
-        myHealthBar.localScale = new Vector3(health / 100f, 1, 1);
+        //I AM THE OWNER OF THIS CUBE
+        if(photonView.IsMine) {
+            health--;
+            //send health over network to all clones
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        if(photonView.IsMine) {
+            stream.SendNext(health);
+        }
+        else {
+            health = (int) stream.ReceiveNext();
+        }
     }
 }
