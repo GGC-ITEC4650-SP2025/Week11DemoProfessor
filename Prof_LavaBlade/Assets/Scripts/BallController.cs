d577ef55c73de3a2accd4654ba0000467b9269c2
Sebastien Siva <ssiva@ggc.edu>
2025-04-15 16:03:03 -0400

    Lecture Ball Color

--- Prof_LavaBlade/Assets/Scripts/BallController.cs
+++ Prof_LavaBlade/Assets/Scripts/BallController.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class BallController : MonoBehaviour
+public class BallController : MonoBehaviourPunCallbacks
 {
+    public Color[] colors;
+    private int currentColor;
+    Renderer myRend;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        myRend.material.color = colors[currentColor];
     }
 
     void OnCollisionEnter(Collision collision)
@@ -25,9 +30,21 @@ public class BallController : MonoBehaviour
             //PlayerController pc = otherGO.GetComponent<PlayerController>();
             //pc.score += 10;
 
+            int points = 10;
+            if(currentColor == 1) {
+                points = -10;
+            }
             //send to every clone of this player an RPC msg
             PhotonView pv = otherGO.GetComponent<PhotonView>();
-            pv.RPC("increaseScore", RpcTarget.AllBuffered, 10);
+            pv.RPC("increaseScore", RpcTarget.AllBuffered, points);
+
+            //update currentColor on all clones of the ball
+            photonView.RPC("setCurrentColor", RpcTarget.All, currentColor + 1);
         }
+    }
+
+    [PunRPC]
+    public void setCurrentColor(int c) {
+        this.currentColor = c % colors.Length; //(Or c % 2)
     }
 }
